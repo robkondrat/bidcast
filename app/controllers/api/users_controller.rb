1ac4063bd5d258caa1492480ecb4ce83043c2683
EatYourSoup7 <r.kondratowicz1@gmail.com>
2020-03-11 12:08:48 -0500

    removed auth for creating user

--- app/controllers/api/users_controller.rb
+++ app/controllers/api/users_controller.rb
@@ -1,6 +1,6 @@
 class Api::UsersController < ApplicationController
-  before_action :authenticate_user, except: [:index, :show]
-  
+  before_action :authenticate_user, except: [:index, :show, :create]
+
   
   def index
     @users = User.all
