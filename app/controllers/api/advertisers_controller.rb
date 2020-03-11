01b626299c81e3c0ac7eec02793941c6f532f19c
EatYourSoup7 <r.kondratowicz1@gmail.com>
2020-03-11 10:57:39 -0500

    added routes and actions for advertiser user type

---
+++ app/controllers/api/advertisers_controller.rb
@@ -0,0 +1,52 @@
+class Api::AdvertisersController < ApplicationController
+  before_action :authenticate_user, except: [:index, :show]
+  
+  
+  def index
+    @advertisers = Advertiser.all
+    render "index.json.jb"
+  end
+
+  def create
+    @advertiser = Advertiser.new(
+                    name: params[:name],
+                    description: params[:description],
+                    email: params[:email],
+                    password: params[:password],
+                    password_confirmation: params[:password_confirmation],
+                    type: params[:type]
+                    )
+
+    if @advertiser.save
+      render json: { message: "Advertiser created successfully" }, status: :created
+    else
+      render json: { errors: advertiser.errors.full_messages }, status: :bad_request
+    end
+
+  end
+
+  def show
+    @advertiser = Advertiser.find(params[:id])
+    render 'show.json.jb'
+  end
+
+  def update
+    @advertiser = Advertiser.find(params[:id])
+
+    @advertiser.name = params[:name] || @advertiser.name
+    @advertiser.description = params[:description] || @advertiser.description
+    @advertiser.email = params[:email] || @advertiser.email
+
+    if @advertiser.save
+      render "show.json.jb"
+    else
+      render json: { errors: @advertiser.errors.full_messages }, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    advertiser = Advertiser.find(params[:id])
+    advertiser.delete
+    render json: {message: "Advertiser ID #{user.id} successfully deleted."}
+  end
+end
