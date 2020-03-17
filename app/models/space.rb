25301ed0bdf711d0841fdf08004468da35e35ded
EatYourSoup7 <r.kondratowicz1@gmail.com>
2020-03-17 11:53:40 -0500

    cleaned up downloaded bootstraps theme

--- app/models/space.rb
+++ app/models/space.rb
@@ -4,12 +4,6 @@ class Space < ApplicationRecord
 
   validates :length, presence: true, numericality: { greater_than: 0, less_than: 210 }
 
-
-
-  # def podcast
-  #   Podcast.find_by(id: self.podcast_id)
-  # end
-
   def expired?
     update(active: false) if created_at >= 3.days.ago && active
     !active
--- lib/tasks/batch.rake
+++ lib/tasks/batch.rake
@@ -4,6 +4,7 @@ namespace :batch do
     # Space.where(active: true).each(&:expired?)
     expired_spaces = Space.where(active: true).select(&:expired?)
     expired_spaces.each { |space| puts "Space #{space.id} has just expired." }
+    # expired_spaces.each { |space| active: false}
   end
 
 end
