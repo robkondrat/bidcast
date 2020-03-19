c9aa091f87e1ec96c3976189a993921ba4118fb0
EatYourSoup7 <r.kondratowicz1@gmail.com>
2020-03-19 14:52:15 -0500

    created Message model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_03_16_195909) do
+ActiveRecord::Schema.define(version: 2020_03_19_195043) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -40,6 +40,13 @@ ActiveRecord::Schema.define(version: 2020_03_16_195909) do
     t.decimal "price", precision: 7, scale: 2
     t.integer "advertiser_id"
     t.integer "space_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "messages", force: :cascade do |t|
+    t.text "body"
+    t.integer "user_id"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
