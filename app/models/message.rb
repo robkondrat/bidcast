3cf7e5f9f51173b0c6ff93addc54f97f627b255e
EatYourSoup7 <r.kondratowicz1@gmail.com>
2020-03-25 12:26:54 -0500

    fixed message styling by user type

--- app/models/message.rb
+++ app/models/message.rb
@@ -1,3 +1,11 @@
 class Message < ApplicationRecord
   belongs_to :user
+
+  def user_type
+    if user.type == "Podcast"
+      return "Podcast"
+    else
+      return "Advertiser"
+    end
+  end
 end
